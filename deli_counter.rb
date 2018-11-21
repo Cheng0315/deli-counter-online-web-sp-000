699f87adb64281aa083159b1c64360f3438e0f75
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 03:24:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(arr)
+  if arr.size == 0
+    puts "The line is currently empty."
+  else
+    people_in_line = ""
+    arr.each do |name|
+      number_in_line = arr.index(name) + 1
+      people_in_line += " #{number_in_line}. #{name}"
+    end
+    puts "The line is currently:#{people_in_line}"
+  end
+end
+
+def take_a_number(arr, person)
+  arr << person
+  number_in_line = arr.length
+  puts "Welcome, #{name}. You are number #{number_in_line} in line."
+end
+
