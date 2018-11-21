f43cdc3c617ae6797a286231a44cd8830f5d6eb7
Cheng Vue <cvu3@ucsc.edu>
2018-11-21 03:33:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(arr)
   if arr.size == 0
     puts "There is nobody waiting to be served!"
   else
-    arr.shift
-    arr
+    serving = arr.shift
+    puts "Currently serving #{serving}."
   end
 end
